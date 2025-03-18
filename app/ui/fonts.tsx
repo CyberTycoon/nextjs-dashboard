9186f5476024950376369fed4859630ec8f53ff7
Cyber Tycoon <silasokanla2006@gmail.com>
2025-03-18 19:24:46 +0100

    fixed font issue

--- app/ui/fonts.tsx
+++
@@ -1,9 +0,0 @@
-import { Lusitana } from 'next/font/google';
-
-// Define the Lusitana font with custom options
-export const lusitana = Lusitana({
-  weight: ['400', '700'], // Regular and Bold weights
-  subsets: ['latin'],     // Latin character subset
-  display: 'swap',        // Use 'swap' to ensure text remains visible during font loading
-  variable: '--font-lusitana', // CSS variable name
-});
